aa8ff955e7dc0e31c0062cdb457e6c09df194046
Shiva_C <73865998+Shiva-C-qwerty@users.noreply.github.com>
2023-04-05 20:00:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
-import streamlit as st
+import subprocess
+from PIL import Image
 
-st.title("Hello")
+st.title(' R x BERT Web App')
 
+st.sidebar.markdown('''
+# About
+This web-app can be used to convert digital image to text.
+We can also use this app to get answers to any questions about the extracted text.
+The main technologies used in this app are:
+- `Tessaract OCR`
+- `BERT Question Answering Model`
+''')
+
+st.subheader('Give your image as input')
+file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
